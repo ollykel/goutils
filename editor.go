3b7f4f48bedb3ccb151e7dec9a8c8f5d4c1c40eb
OKelt <oakelton@gmail.com>
2019-07-19 12:47:19 -0700

    added func Edit to resources/editor.go

--- editor.go
+++ editor.go
@@ -8,10 +8,16 @@ package goutils
  */
 
 import (
+	"fmt"
 	"os"
 	"log"
 	"io/ioutil"
 )
+
+type EditorConfig struct {
+	Name		string
+	Flags		[]string
+}//-- end EditorConfig struct
 
 func getEditor (def string) (path string) {
 	editor := os.Getenv("EDITOR"); if editor == "" {
@@ -20,6 +26,27 @@ func getEditor (def string) (path string) {
 	path = Which(editor)
 	return
 }
+
+func Edit (init []byte, cfg *EditorConfig) (output []byte, err error) {
+	tmp, err := ioutil.TempFile("", "goedit_*.txt"); if err != nil { return }
+	fName := tmp.Name()
+	defer tmp.Close(); defer os.Remove(fName)
+	tmp.Write(init)
+	editor := Which(cfg.Name); if editor == "" {
+		return nil, fmt.Errorf("unrecognized editor '%s'", cfg.Name)
+	}
+	argv := make([]string, len(cfg.Flags) + 2)
+	argv[0] = cfg.Name
+	copy(argv[1:], cfg.Flags)
+	argv[len(argv) - 1] = tmp.Name()
+	proc, err := os.StartProcess(editor, argv,
+		&os.ProcAttr{Files: []*os.File{os.Stdin, os.Stdout, os.Stderr}})
+	if err != nil { return }
+	_, err = proc.Wait(); if err != nil { return }
+	tmp.Seek(0, os.SEEK_SET)
+	output, err = ioutil.ReadAll(tmp); if err != nil { return }
+	return
+}//-- end func Edit
 
 func CallEditor (defEditor string, template []byte) (output []byte) {
 	tmp, err := ioutil.TempFile("", "goedit_*.md"); if err != nil {
