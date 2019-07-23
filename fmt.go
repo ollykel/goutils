d4221bcdfd992a7692eca079f689634de3a43c1f
OKelt <oakelton@gmail.com>
2019-07-23 16:58:58 -0700

    created Replacer

--- fmt.go
+++ fmt.go
@@ -37,3 +37,51 @@ func ParseVars (demarc byte, vars map[string]string, orig ...string) (parsed []s
 	return
 }
 
+type Replacer struct {
+	demarc		byte
+	defs		map[string]string
+	rep			*strings.Replacer
+}//-- end type Replacer
+
+func (rep *Replacer) refresh () {
+	pairs := make([]string, 0, len(rep.defs) << 2)
+	for k, v := range rep.defs {
+		varName := make([]rune, len(k) + 1)
+		varName[0] = rune(rep.demarc)
+		copy(varName[1:], []rune(k))
+		pairs = append(pairs, string(varName), v)
+	}
+	rep.rep = strings.NewReplacer(pairs...)
+}//-- end func Replacer.refresh
+
+func NewReplacer (demarc byte, defs map[string]string) (rep Replacer) {
+	rep.demarc, rep.defs = demarc, make(map[string]string)
+	for k, v := range defs {
+		rep.defs[k] = v
+	}
+	rep.refresh()
+	return
+}//-- end func NewReplacer
+
+func (rep *Replacer) Get (key string) (val string, exists bool) {
+	val, exists = rep.defs[key]
+	return
+}//-- end func Replacer.Get
+
+func (rep *Replacer) Set (key, val string) {
+	rep.defs[key] = val
+	rep.refresh()
+}//-- end func Replacer.Set
+
+func (rep *Replacer) Remap (mapper func(string)string) (self *Replacer) {
+	for k, v := range rep.defs {
+		rep.defs[k] = mapper(v)
+	}//-- end for range rep.defs
+	rep.refresh()
+	return rep
+}//-- end func Replacer.Remap
+
+func (rep *Replacer) Replace (orig string) string {
+	return rep.rep.Replace(orig)
+}//-- end func Replacer.Replace
+
